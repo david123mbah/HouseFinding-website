aaabd209cd478b7844e88a6cbd81c7dd558b0b4d
david123mbah <davidmbah1234@gmail.com>
2024-05-14 19:30:00 +0100

    nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import './Hero.css'
+import './index.css'
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerwidth flexCenter hero-Container " >
-        <div className="hero-left">
-          left section
+        {/* left side */}
+        <div className=" FlexColstart hero-left">
+          <div className="hero-title">
+            <h1>
+              Find Your <br/>Dream Home <br/>Today
+            </h1>
+          </div>
+          <div className="flexColstart hero-des">
+            <span>
+             are you ready to strat the search for your homr? Look no futher
+            </span>
+            <span>
+             Move-in is Here for you.
+            </span>
+            <div className="search-bar">
+              Search bar
+            </div>
+          </div>
         </div>
+        {/* right side */}
         <div className="flexCenter hero-right">
           <div className="image-container">
-            <img src="./hero-image" alt="" />
+            <img src="./hero-image.png" alt="" />
           </div>
         </div>
 
